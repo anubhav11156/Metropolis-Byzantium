a3346c0fda7f2d8140c01cfc7070914cea57a669
Anubhav Kumar <anubhav11697@gmail.com>
2022-11-06 16:37:35 +0530

    ADD: Hero Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,9 @@ function Home() {
     return (
         <Container>
           <Hero />
+        { /* <div className="frame-div">
+            <iframe className="iframe-1" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2Fs8bhmu6wA3wQiPbmXhn5YK%2FFor-Metropolis%3Fnode-id%3D0%253A1"></iframe>
+          </div> */}
         </Container>
     )
 }
@@ -14,6 +17,31 @@ function Home() {
 export default Home
 
 const Container=styled.div`
-  background-color: lightblue;
   height: 100vh;
+
+  .spacer {
+    aspect-ratio: 960/300;
+    width: 100%;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    img {
+      width: 100%;
+    }
+  }
+
+  .frame-div {
+
+    .iframe-1 {
+      border: 1px solid rgba(0, 0, 0, 0.1);
+      width:800px;
+      height:450px;
+    }
+
+  }
+
 `
